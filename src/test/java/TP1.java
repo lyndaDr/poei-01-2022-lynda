ecf2214f83d2c63a07cc7233eb065a373db5031a
Lynda DROUICHE <lyndadrouiche96@gmail.com>
2022-01-05 12:24:18 +0100

    Added implicit wait

--- src/test/java/TP1.java
+++ src/test/java/TP1.java
@@ -7,11 +7,18 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class TP1 {
     WebDriver driver;
     @BeforeMethod
     public void setup(){
         driver = new ChromeDriver();
+
+
+        //Implicite wait 2sc ici
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
         driver.get("https://www.amazon.fr");
         //pour max la page
         driver.manage().window().maximize();
@@ -27,45 +34,27 @@ public class TP1 {
         barreRecherche.sendKeys("machine a raclette");
         barreRecherche.sendKeys(Keys.ENTER);
 
-        //selectionner un element par Nom exemple de plus
-        // driver.findElement(By.name("field-keywords"));
     }
     @Test
     public void test2 () {
 
-        
         //Afficher barre de recherche et chercher un produit et clicker sur entrer
         WebElement barreRecherche= driver.findElement(By.id("twotabsearchtextbox"));
         barreRecherche.sendKeys("machine a raclette");
         barreRecherche.sendKeys(Keys.ENTER);
 
-        //selectionner un element par Nom exemple de plus
-        // driver.findElement(By.name("field-keywords"));
-
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         //1er Resultat de la recherche
         WebElement resultatRecherche= driver.findElement(By.cssSelector("[data-cel-widget='search_result_1']"));
         resultatRecherche.click();
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
         //Ajouter au panier
         WebElement buttonPanier= driver.findElement(By.id("add-to-cart-button"));
         buttonPanier.click();
-
     }
 
     @AfterMethod
     public void teardown() {
         driver.quit();
-
     }
 
 }
